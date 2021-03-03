4b6c8ada9a9e3d65e218a0717ee7a4f69117514b
wangfuming <wangfuming@bytedance.com>
2021-03-03 16:16:30 +0800

    :new: add listener on tree

--- src/main/java/icybee/solver/gui/SolverResult.java
+++ src/main/java/icybee/solver/gui/SolverResult.java
@@ -6,55 +6,65 @@ import icybee.solver.nodes.GameActions;
 import icybee.solver.nodes.GameTreeNode;
 
 import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import java.util.List;
 
 public class SolverResult {
     public JPanel resultPanel;
-    private JTable table1;
+    private JTable strategy_table;
     private JScrollPane tree_pane;
     private JTree game_tree_field;
 
     GameTree game_tree;
     GameTreeNode root;
     GameTreeNode.GameRound round;
+
+    class NodeDesc{
+        GameTreeNode node;
+        GameActions last_action;
+        int action_ind;
+        NodeDesc(GameTreeNode node,GameActions last_action,int action_ind){
+            this.node = node;
+            this.last_action = last_action;
+            this.action_ind = action_ind;
+        }
+
+        @Override
+        public String toString() {
+            if(this.last_action == null) {
+                return String.format("%s begin", GameTreeNode.gameRound2String(this.node.getRound()));
+            }else{
+                return String.format("p%d %s",((ActionNode) this.node).getPlayer(),last_action.toString());
+            }
+        }
+    }
+
     SolverResult(GameTree game_tree,GameTreeNode root){
         this.game_tree = game_tree;
         this.root = root;
         this.round = root.getRound();
-        /*
-        DefaultMutableTreeNode treenode =new DefaultMutableTreeNode("教师学历信息");
-        String Teachers[][]=new String[3][];
-        Teachers[0]=new String[]{"王鹏","李曼","韩小国","穆保龄","尚凌云","范超峰"};
-        Teachers[1]=new String[]{"胡会强","张春辉","宋芳","阳芳","朱山根","张茜","宋媛媛"};
-        Teachers[2]=new String[]{"刘丹","张小芳","刘华亮","聂来","吴琼"};
-        String gradeNames[]={"硕士学历","博士学历","博士后学历"};
-        DefaultMutableTreeNode node=null;
-        DefaultMutableTreeNode childNode=null;
-        int length=0;
-
-        for(int i=0;i<3;i++)
-        {
-            length=Teachers[i].length;
-            node=new DefaultMutableTreeNode(gradeNames[i]);
-            for (int j=0;j<length;j++)
-            {
-                childNode = new DefaultMutableTreeNode(Teachers[i][j]);
-                node.add(childNode);
-            }
-            treenode.add(node);
-        }
-         */
-        DefaultMutableTreeNode treenode = new DefaultMutableTreeNode(String.format("%s begin",GameTreeNode.gameRound2String(root.getRound())));
+        DefaultMutableTreeNode treenode = new DefaultMutableTreeNode();
+        treenode.setUserObject(new NodeDesc(root,null,0));
         reGenerateTree(this.root,treenode);
         JTree jtree_field = new JTree(treenode);
         game_tree_field.setModel(jtree_field.getModel());
-    }
 
-    class NodeDesc{
-        NodeDesc(){
+        game_tree_field.addTreeSelectionListener(new TreeSelectionListener() {
+            public void valueChanged(TreeSelectionEvent e) {
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+                        game_tree_field.getLastSelectedPathComponent();
 
-        }
+                /* if nothing is selected */
+                if (node == null) return;
+                Object nodeInfoObject = node.getUserObject();
+                NodeDesc nodeinfo = (NodeDesc) nodeInfoObject;
+                if(nodeinfo.last_action == null) return;
+                System.out.println(nodeinfo.last_action.toString());
+
+            }
+        });
     }
 
     void reGenerateTree(GameTreeNode node,DefaultMutableTreeNode parent){
@@ -68,7 +78,9 @@ public class SolverResult {
             for(int i = 0;i < childs.size();i ++){
                 GameTreeNode one_child = childs.get(i);
                 GameActions one_action = actions.get(i);
-                DefaultMutableTreeNode one_tree_child = new DefaultMutableTreeNode(String.format("p%d %s",((ActionNode) node).getPlayer(),one_action.toString()));
+                DefaultMutableTreeNode one_tree_child = new DefaultMutableTreeNode();
+
+                one_tree_child.setUserObject(new NodeDesc(root,one_action,i));
                 parent.add(one_tree_child);
                 reGenerateTree(one_child,one_tree_child);
             }
