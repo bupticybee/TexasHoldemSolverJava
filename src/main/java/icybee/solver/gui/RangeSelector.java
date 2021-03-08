454c23753bfae645658d87e4f39ab0e203063896
wangfuming <wangfuming@bytedance.com>
2021-03-08 20:56:10 +0800

    :new: start construction of range selection panel

---
+++ src/main/java/icybee/solver/gui/RangeSelector.java
@@ -0,0 +1,16 @@
+package icybee.solver.gui;
+
+import javax.swing.*;
+
+public class RangeSelector {
+
+    private JTextArea range_text;
+    private JTable range_table;
+    private JTree tree1;
+    private JPanel range_selector_main_panel;
+    private JPanel range_table_panel;
+    private JSlider slider1;
+
+    public RangeSelector() {
+    }
+}
